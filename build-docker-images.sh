ff39737fe78b0291c513944a43e19442a1bbb960
Michele Adduci <michele.adduci@openlimit.com>
2019-09-13 16:22:39 +0200

    Updated scripts, removed old SDK and ARM related libraries, made unique extended msvc image with all the tools

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -6,11 +6,8 @@ docker-compose up -d
 # Build base Docker Wine image
 docker build --no-cache -t docker-wine:4.0-stable -f base/Dockerfile.wine base/
 
-# Build msvc Docker Image
-docker build --no-cache -t docker-msvc:16.2-2019 -f msvc-cpp/Dockerfile.msvc msvc-cpp/
-
 # Build extended Docker Image (with CMake, Wix and Conan)
-docker build --no-cache -t docker-msvc-extended:16.2-2019 -f msvc-cpp-extended/Dockerfile.tools msvc-cpp-extended/
+docker build --no-cache -t docker-msvc-extended:16.2-2019 -f msvc-cpp-extended/Dockerfile msvc-cpp-extended/
 
 # Shutdown python server serving packages
 docker-compose down -v
