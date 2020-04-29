17657a14d9e09ac7538b5c4ffaae717f13b14c5d
Michele Adduci <adduci.michele@gmail.com>
2020-04-29 14:30:16 +0200

    Change name of base image

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Execute a Python server serving the downloaded packages
 docker-compose up -d
 
 # Build base Docker Wine image
-docker build --no-cache -t docker-wine:5.0-stable -f base/Dockerfile base/
+docker build  --no-cache  -t docker-wine:5-staging -f base/Dockerfile base/
 
 # Build extended Docker Image (with CMake, Wix and Conan)
 docker build --no-cache -t docker-wine-msvc:16.5-2019 -f msvc-cpp/Dockerfile msvc-cpp/
