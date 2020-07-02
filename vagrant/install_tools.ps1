e7358e829c03865055d3d8fa929ebcbc66f85bef
Michele Adduci <adduci.michele@gmail.com>
2020-07-02 12:11:38 +0200

    Updated Conan version and MSVC Version

--- vagrant/install_tools.ps1
+++ vagrant/install_tools.ps1
@@ -29,8 +29,8 @@ $cmake_zip_output_path = "$packages_directory\CMake"
 ####################################
 # Define Conan information
 ####################################
-$conan_version = "1.26.1"
-$conan_version_classifier = "win-64_1_26_1"
+$conan_version = "1.27.0"
+$conan_version_classifier = "win-64_1_27_0"
 $conan_installer_url = "https://github.com/conan-io/conan/releases/download/$conan_version/conan-$conan_version_classifier.exe"
 $conan_installer_exe = "$working_directory\conan.exe"
 $conan_install_path = "C:\Program Files\Conan"
