442700d7267fddd766a094d147d3e541239fb1a9
Paolo Dastoli <paolo.dastoli@gmail.com>
2023-12-30 14:17:08 +0100

    Added Git to the toolchain. To guarantee access via http authentication simply create the file 	home/wine/.wine/drive_c/users/wine/.git-credentials containing 	https://USER:$PASSWORD@HOST

--- msvc-cpp/x64.bat
+++ msvc-cpp/x64.bat
@@ -5,11 +5,13 @@ set CONAN_REVISIONS_ENABLED=1
 set CONAN_USE_ALWAYS_SHORT_PATHS=1
 set CONAN_NON_INTERACTIVE=1
 
+set CMAKE_GENERATOR=Ninja
+
 set ExtensionSdkDir=C:\Tools\SDK\10\ExtensionSDKs
 set INCLUDE=C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\atlmfc\include;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\include;C:\Tools\SDK\10\include\10.0.19041.0\ucrt;C:\Tools\SDK\10\include\10.0.19041.0\shared;C:\Tools\SDK\10\include\10.0.19041.0\um;C:\Tools\SDK\10\include\10.0.19041.0\winrt;C:\Tools\SDK\10\include\10.0.19041.0\cppwinrt
 set LIB=C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\atlmfc\lib\x64;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\lib\x64;C:\Tools\SDK\10\lib\10.0.19041.0\ucrt\x64;C:\Tools\SDK\10\lib\10.0.19041.0\um\x64
 set LIBPATH=C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\atlmfc\lib\x64;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\lib\x64;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\lib\x86\store\references;C:\Tools\SDK\10\UnionMetadata\10.0.19041.0;C:\Tools\SDK\10\References\10.0.19041.0;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\atlmfc\lib\x86;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\lib\x86;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\lib\x86\store\references;C:\Tools\SDK\10\UnionMetadata\10.0.19041.0;C:\Tools\SDK\10\References\10.0.19041.0;
-set PATH=C:\Tools\Conan\conan;C:\Tools\CMake\bin;C:\Tools\Wix;C:\Tools\Ninja;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64;C:\Tools\SDK\10\bin\10.0.19041.0\x64;C:\Tools\SDK\10\bin\x64;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;
+set PATH=C:\Tools\Git\cmd;C:\Tools\Conan\conan;C:\Tools\CMake\bin;C:\Tools\Wix;C:\Tools\Ninja;C:\Tools\VS2019\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64;C:\Tools\SDK\10\bin\10.0.19041.0\x64;C:\Tools\SDK\10\bin\x64;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;
 set PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC
 set Platform=x64
 set PROCESSOR_ARCHITECTURE=AMD64
