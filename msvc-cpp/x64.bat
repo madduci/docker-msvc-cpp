9b6bbd55c8a034effcdf2afb74f6b37278c09ec8
Michele Adduci <adduci@tutanota.com>
2024-01-25 15:40:30 +0100

    Fixing issues with detecting msvc 17

--- msvc-cpp/x64.bat
+++ msvc-cpp/x64.bat
@@ -1,6 +1,5 @@
 @echo off
 
-set CONAN_CMAKE_GENERATOR=Ninja
 set CONAN_REVISIONS_ENABLED=1
 set CONAN_USE_ALWAYS_SHORT_PATHS=1
 set CONAN_NON_INTERACTIVE=1
@@ -9,15 +8,15 @@ set CMAKE_GENERATOR=Ninja
 
 set ExtensionSdkDir=C:\Tools\SDK\10\ExtensionSDKs
 set INCLUDE=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\include;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\include;C:\Tools\VS2022\VC\Auxiliary\VS\include;C:\Tools\SDK\10\include\10.0.20348.0\ucrt;C:\Tools\SDK\10\include\10.0.20348.0\um;C:\Tools\SDK\10\include\10.0.20348.0\shared;C:\Tools\SDK\10\include\10.0.20348.0\\winrt;C:\Tools\SDK\10\include\10.0.20348.0\cppwinrt
-set LIB=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\lib\x86;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x86;C:\Tools\SDK\10\lib\10.0.20348.0\ucrt\x86;C:\Tools\SDK\10\\lib\10.0.20348.0\um\x86
-set LIBPATH=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\lib\x86;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x86;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x86\store\references;C:\Tools\SDK\10\UnionMetadata\10.0.20348.0;C:\Tools\SDK\10\References\10.0.20348.0
+set LIB=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x64;C:\Tools\SDK\10\lib\10.0.20348.0\ucrt\x64;C:\Tools\SDK\10\\lib\10.0.20348.0\um\x64
+set LIBPATH=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x64\store\references;C:\Tools\SDK\10\UnionMetadata\10.0.20348.0;C:\Tools\SDK\10\References\10.0.20348.0
 set PATH=C:\Tools\Git\cmd;C:\Tools\Conan\conan;C:\Tools\CMake\bin;C:\Tools\Wix;C:\Tools\Ninja;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\bin\HostX64\x64;C:\Tools\SDK\10\bin\10.0.20348.0\x64;C:\Tools\SDK\10\bin\x64;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;
 set PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC
 set Platform=x64
 set PROCESSOR_ARCHITECTURE=AMD64
 set UCRTVersion=10.0.20348.0
 set UniversalCRTSdkDir=C:\Tools\SDK\10\
-set VS160COMNTOOLS=C:\Tools\Common7\Tools\
+set VS170COMNTOOLS=C:\Tools\Common7\Tools\
 set VCINSTALLDIR=C:\Tools\VS2022\VC\
 set VCToolsInstallDir=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\
 set VCToolsRedistDir=C:\Tools\VS2022\VC\Redist\MSVC\14.38.33130\
