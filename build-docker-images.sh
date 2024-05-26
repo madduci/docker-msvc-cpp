89ab97a97743a3dfa9455864aec5d29a9497e432
Michele Adduci <adduci@tutanota.com>
2024-05-26 09:48:55 +0200

    Updated tools to their latest version, bump to Ubuntu 24.04

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -4,4 +4,4 @@
 docker build  --no-cache  -t madduci/docker-wine:9-stable -f base/Dockerfile base/
 
 # Build extended Docker Image (with CMake, Wix and Conan)
-docker build --no-cache -t madduci/docker-wine-msvc:17.8-2022 -f msvc-cpp/Dockerfile .
+docker build --no-cache -t madduci/docker-wine-msvc:17.10-2022 -f msvc-cpp/Dockerfile .
